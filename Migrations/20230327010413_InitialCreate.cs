8b9f02ecdf2373470196fbe3d8d5798ff0511034
juanhernandezdeveloper <jualherac@gmail.com>
2023-03-27 21:26:40 -0500

    Add data in customers and movies lists

--- Migrations/20230327010413_InitialCreate.cs
+++ Migrations/20230327010413_InitialCreate.cs
@@ -10,61 +10,61 @@ namespace Movies_API_WS.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.CreateTable(
-            //    name: "Customer",
-            //    columns: table => new
-            //    {
-            //        CustomerId = table.Column<int>(type: "int", nullable: false),
-            //        Age = table.Column<int>(type: "int", nullable: false),
-            //        Gender = table.Column<string>(type: "char(1)", unicode: false, fixedLength: true, maxLength: 1, nullable: false),
-            //        Occupation = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Customer_CustomerId", x => x.CustomerId);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "char(1)", unicode: false, fixedLength: true, maxLength: 1, nullable: false),
+                    Occupation = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer_CustomerId", x => x.CustomerId);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "Movie",
-            //    columns: table => new
-            //    {
-            //        MovieId = table.Column<int>(type: "int", nullable: false),
-            //        Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Movie_MovieId", x => x.MovieId);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie_MovieId", x => x.MovieId);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "Rating",
-            //    columns: table => new
-            //    {
-            //        CustomerId = table.Column<int>(type: "int", nullable: false),
-            //        MovieId = table.Column<int>(type: "int", nullable: false),
-            //        Rating = table.Column<int>(type: "int", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Rating", x => new { x.CustomerId, x.MovieId });
-            //        table.ForeignKey(
-            //            name: "FK_Rating_Customer",
-            //            column: x => x.CustomerId,
-            //            principalTable: "Customer",
-            //            principalColumn: "CustomerId",
-            //            onDelete: ReferentialAction.Cascade);
-            //        table.ForeignKey(
-            //            name: "FK_Rating_Movie",
-            //            column: x => x.MovieId,
-            //            principalTable: "Movie",
-            //            principalColumn: "MovieId",
-            //            onDelete: ReferentialAction.Cascade);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Rating",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rating", x => new { x.CustomerId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_Rating_Customer",
+                        column: x => x.CustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rating_Movie",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
-            //migrationBuilder.CreateIndex(
-            //    name: "IX_Rating_MovieId",
-            //    table: "Rating",
-            //    column: "MovieId");
+            migrationBuilder.CreateIndex(
+                name: "IX_Rating_MovieId",
+                table: "Rating",
+                column: "MovieId");
         }
 
         /// <inheritdoc />
--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Movies_API_WS.Models;
+using System.Data;
+using System.Reflection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +29,103 @@ app.MapGet("/Rating", (MoviesApiContext context) =>
 {
     return context.Ratings.Take(100).ToHashSet<Rating>();
 });
+
+string connectionString = "Data Source = localhost\\SQLEXPRESS; Database = MoviesAPI; Integrated Security = True; TrustServerCertificate = True;";
+
+// Call InsertUsers function
+InsertCustomers(connectionString);
+
+// Call InsertMovies function
+InsertMovies(connectionString);
+
+// Call InsertRatings function
+InsertRatings(connectionString);
+
+// InsertUsers function
+static void InsertCustomers(string connectionString)
+{
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        using (StreamReader reader = new StreamReader("StoredProcedures/Customers.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split('|');
+                int customerId = int.Parse(values[0]);
+                int age = int.Parse(values[1]);
+                string gender = values[2];
+                string occupation = values[3];
+
+                using (SqlCommand command = new SqlCommand("InsertCustomer", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@CustomerId", customerId);
+                    command.Parameters.AddWithValue("@Age", age);
+                    command.Parameters.AddWithValue("@Gender", gender);
+                    command.Parameters.AddWithValue("@Occupation", occupation);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
+
+// InsertMovies function
+static void InsertMovies(string connectionString)
+{
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        using (StreamReader reader = new StreamReader("StoredProcedures/Movies.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split('|');
+                int movieId = int.Parse(values[0]);
+                string name = values[1];
+
+                using (SqlCommand command = new SqlCommand("InsertMovie", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@MovieId", movieId);
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
+
+// InsertRatings function
+static void InsertRatings(string connectionString)
+{
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+        using (StreamReader reader = new StreamReader("StoredProcedures/Ratings.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                string[] values = line.Split('\t');
+                int customerId = int.Parse(values[0]);
+                int movieId = int.Parse(values[1]);
+                int rating = int.Parse(values[2]);
+
+                using (SqlCommand command = new SqlCommand("InsertRating", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@CustomerId", customerId);
+                    command.Parameters.AddWithValue("@MovieId", movieId);
+                    command.Parameters.AddWithValue("@Rating1", rating);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
 
 app.Run();
